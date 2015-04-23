8d3873063a49a7873d93d9e3f17172e3cdf47e01
Stephen J. Eglen <sje30@cam.ac.uk>
2015-04-23 09:51:07 +0100

    Simple "readme" to help the user get started.

---
+++ vignettes/rstudio.R
@@ -0,0 +1,11 @@
+## This package contains the files required to regenerate
+## the 2015 review article by Stephen Eglen
+
+## To see the vignette, run the following line in the console:
+vignette("eglen2015")
+
+## To see the source file for the paper:
+eglen2015:::edit()
+
+## To rebuild the vignette from the sources:
+tools::buildVignettes("eglen2015")
